d52914d0438e3c056623493438c8c5a2be3775b1
sybrk <snn.yll@gmail.com>
2024-01-17 22:58:37 +0300

    works well

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 //globals
-let page = 1;
+
+let apiResult = {};
 
 //objects
 let data = {
-    getData: function (page = 1, cb) {
+    // I will use url parameter to get pages dynamically
+    getData: function (url = `https://rickandmortyapi.com/api/character`, cb) {
         var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+        // we need to set this to false to avoid errors in CORS.
+        xhr.withCredentials = false;
 
         xhr.addEventListener("readystatechange", function() {
             if(this.readyState === 4) {
@@ -13,14 +16,14 @@ let data = {
             }
         });
 
-        xhr.open("GET", `http://localhost:5500/data${page}.json`);
+        xhr.open("GET", url);
         xhr.send();
 
     },
-    getDataAsync : function(page=1){
+    getDataAsync : function(){
         return new Promise((resolve, reject )=> {
             try {
-                data.getData(page, function(result){
+                data.getData(function(result){
                     resolve(result);
                 });    
             } catch (error) {
@@ -32,49 +35,77 @@ let data = {
 
 };
 
-
-
-
-function makeDivForCharacterCard(characterData){
-    let character_card_template = `<div class="character-card">
-        <img src="${characterData.image}" />
-        <h2>${characterData.name}(${characterData.gender})</h2>
-    </div>`;
-
-    return character_card_template;
-}
-
-
-function fillContainerWithData(dataArray){
-    let my_container = document.getElementsByClassName('container')[0]
-    let container_html = '';
-    for (let i = 0; i < dataArray.results.length; i++) {
-        const characterData = dataArray.results[i];
-        container_html += makeDivForCharacterCard(characterData);
+let domHelper = {
+    createDOMElement: function (tagname, className, id) {
+        let createElement = document.createElement(tagname);
+        if (className !== undefined) {
+            createElement.classList.add(className);
+        }
+        if (id !== undefined) {
+            createElement.id = id;
+        }
+        return createElement;
+    },
+    appendElement: function (parent, child) {
+        parent.appendChild(child);
     }
-    my_container.innerHTML = container_html;
 }
 
-async function generateMylist(page=1){
-    let result = await data.getDataAsync(page);
-    let currentData = JSON.parse(result);
-    fillContainerWithData(currentData);
-    // data.getData(page, function(result){
-    //     let currentData = JSON.parse(result);
-    //     fillContainerWithData(currentData);
-    // });
-}
-async function next() {
-    page++;
-    await generateMylist(page);
+let creatingPage = {
+    makeDivForCharacterCard: function(characterData){
+
+        let myDiv = domHelper.createDOMElement("div", "character-card");
+        myDiv.classList.add("imagecards")
+        let myImg = domHelper.createDOMElement("img");
+        myImg.setAttribute("src", characterData.image);
+        let myH2 = domHelper.createDOMElement("h2");
+        myH2.textContent = `${characterData.name} (${characterData.gender})`;
+        domHelper.appendElement(myDiv, myImg);
+        domHelper.appendElement(myDiv, myH2);
+    
+        return myDiv;
+    },
+    fillContainerWithData: function(dataArray){
+        let _self = this;
+        let myContainer = document.getElementsByClassName('container')[0]
+        // we clean container before adding new content.
+        myContainer.innerHTML = "";
+        for (let i = 0; i < dataArray.results.length; i++) {
+            const characterData = dataArray.results[i];
+            let myDiv = _self.makeDivForCharacterCard(characterData);
+            domHelper.appendElement(myContainer, myDiv);
+        }
+    },
+    generateMylist: function (url){
+        let _self = this;
+        data.getData(url, function(result){
+            apiResult = JSON.parse(result);
+            _self.fillContainerWithData(apiResult);
+            // we disable buttons if next/prev url from api info is null.
+            let nextButton = document.getElementById("nextButton");
+            let prevButton = document.getElementById("previousButton");
+            // we check next/previous urls dynamically from api info.
+            apiResult.info.prev === null ? prevButton.disabled = true : prevButton.disabled = false;
+            apiResult.info.next === null ? nextButton.disabled = true : nextButton.disabled = false;
+
+        });
+        
+    }
 }
 
-async function prev() {
-    page--;
-    await generateMylist(page);
+
+function next() {
+    
+    creatingPage.generateMylist(apiResult.info.next);
+}
+
+function prev() {
+
+    creatingPage.generateMylist(apiResult.info.prev);
 }
 
 //document events
-document.addEventListener('DOMContentLoaded',async function () {
-    await generateMylist();
+document.addEventListener('DOMContentLoaded', function () {
+    creatingPage.generateMylist();
+    
 })
